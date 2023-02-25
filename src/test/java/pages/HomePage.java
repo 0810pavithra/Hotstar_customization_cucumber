011839223277fff0706b09da2543c888b2e37135
Muthusamy <pavithra.b.muthusamy@capgemini.com>
2023-02-25 23:48:27 +0530

    Modified files are Added

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -15,77 +15,96 @@ public class HomePage {
         this.driver = driver;
         PageFactory.initElements(driver,this);
     }
+
+
     //testcase1
     @FindBy(id = "searchField")
     WebElement searchField;
+
     public WebElement getSearchField() {
         return searchField;
     }
-
     @FindBy(xpath = "//span[contains(text(), 'movies')]")
     WebElement searchResult;
+
     public WebElement getSearchResult() {
         return searchResult;
     }
+
     //testcase2
     @FindBy(xpath = "//div[@id='btn-hamburger']")
      WebElement menu;
+
     public WebElement getMenu() {
         return menu;
     }
     @FindBy(linkText = ("Channels"))
     WebElement Channels;
+
     public WebElement getChannels() {
         return Channels;
     }
     @FindBy(xpath = "//article[@class=\"channel-card normal\"]//a[1]")
     WebElement searchChannels;
+
     public WebElement getSearchChannels() {
         return searchChannels;
     }
+
     //testcase3
     @FindBy(linkText = "Languages")
     WebElement Languages;
+
     public WebElement getLanguages() {
         return Languages;
     }
     @FindBy(xpath = ".//span[text()='Hindi']")
     WebElement searchLanguage;
+
     public WebElement getSearchLanguage() {
         return searchLanguage;
     }
+
     //testcase4
     @FindBy(linkText = ("Genres"))
      WebElement Genres;
+
     public WebElement getGenres() {
         return Genres;
     }
     @FindBy(xpath=".//span[text()='DRAMA']")
      WebElement movies;
+
     public WebElement getMovies() {
         return movies;
     }
+
     //testcase5
     @FindBy(xpath = "//div[@class ='navigations']/ul/li[5]/div//a[@class ='nav-link']")
     WebElement kids;
+
     public WebElement getKids() {
         return kids;
     }
     @FindBy(id = "searchField")
     WebElement searchKids;
+
     public WebElement getSearchKids() {
         return searchKids;
     }
     @FindBy(xpath = ".//span[contains(text(), 'princess')]")
     WebElement searchKidsResult;
+
     public WebElement getSearchKidsResult() {
         return searchKidsResult;
     }
     @FindBy(xpath = "//button[@type ='button']")
     WebElement returnHome;
+
     public WebElement getReturnHome() {
         return returnHome;
     }
+
     //testcase 6
     @FindBy(xpath = "//div[@class='signIn']")
     WebElement searchLogin;
